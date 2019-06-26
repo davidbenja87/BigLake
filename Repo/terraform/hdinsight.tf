180d6390602a42c6f9d216b021784e394eeb22cd
Sugumar <sugumarb@b2b.regn.net>
2019-06-26 16:42:09 +0100

    create storage account using template language

--- Repo/terraform/hdinsight.tf
+++ Repo/terraform/hdinsight.tf
@@ -1,12 +1,9 @@
-
-
-# # resource "azurerm_storage_container" "example" {
-# #   name                  = "hdinsight"
-# #   resource_group_name   = "${azurerm_resource_group.example.name}"
-# #   storage_account_name  = "${azurerm_data_lake_store.example.name}"
-# #   container_access_type = "private"
-# # }
-
+# resource "azurerm_storage_container" "example" {
+#   name                  = "hdinsight"
+#   resource_group_name   = "${azurerm_resource_group.example.name}"
+#   storage_account_name  = "${azurerm_data_lake_store.example.name}"
+#   container_access_type = "private"
+# }
 # resource "azurerm_hdinsight_spark_cluster" "example" {
 #   name                = "example-hdicluster"
 #   resource_group_name = "${azurerm_resource_group.example.name}"
@@ -16,37 +13,31 @@
 # #   dependsOn = [
 # #                 "Microsoft.Storage/storageAccounts/test"
 # #             ]
-
 #   component_version {
 #     spark = "2.3"
 #   }
-
 #   gateway {
 #     enabled  = true
 #     username = "acctestusrgw"
 #     password = "TerrAform123!"
 #   }
-
 #   storage_account {
-#     # storage_container_id = "${azurerm_storage_container.example.id}"
-#     # storage_account_key  = "${azurerm_data_lake_store.example.id}"
+#     storage_container_id = "${azurerm_storage_container.example.id}"
+#     storage_account_key  = "${azurerm_data_lake_store.example.id}"
 #     is_default           = false
 #   }
-
 #   roles {
 #     head_node {
 #       vm_size  = "Standard_A3"
 #       username = "acctestusrvm"
 #       password = "AccTestvdSC4daf986!"
 #     }
-
 #     worker_node {
 #       vm_size               = "Standard_A3"
 #       username              = "acctestusrvm"
 #       password              = "AccTestvdSC4daf986!"
 #       target_instance_count = 1
 #     }
-
 #     zookeeper_node {
 #       vm_size  = "Medium"
 #       username = "acctestusrvm"
--- Repo/terraform/variables.tf
+++ Repo/terraform/variables.tf
@@ -1,17 +1,20 @@
 variable "ARM_SUBSCRIPTION_ID" {
-	default ="974668b8-821d-4cc2-a84d-c81a7733f464"
+  default = "974668b8-821d-4cc2-a84d-c81a7733f464"
 }
 
 variable "ARM_CLIENT_ID" {
-	default ="f0a5d79c-4832-48f9-b6e7-5d46968bb283"
+  default = "f0a5d79c-4832-48f9-b6e7-5d46968bb283"
 }
 
 variable "ARM_CLIENT_SECRET" {
-	default ="e549ac29-a0d8-48ce-8d4f-b668b2d661a9"
+  default = "e549ac29-a0d8-48ce-8d4f-b668b2d661a9"
 }
 
 variable "ARM_TENANT_ID" {
-	default ="c8fedfb4-d804-457b-9745-362581de2e4c"
-	# default ="b60c9401-2154-40aa-9cff-5e3d1a20085d"
+  default = "c8fedfb4-d804-457b-9745-362581de2e4c"
+  # default ="b60c9401-2154-40aa-9cff-5e3d1a20085d"
 }
 
+variable "storageaccountname" {
+  default = "biglakestoragepoctest"
+}
