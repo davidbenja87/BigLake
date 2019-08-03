1749c0544e8e8dfdd6318dda548447bcc5facc8a
davidbenja87 <david.benja.87@gmail.com>
2019-08-03 09:25:50 +0100

    first spark pgm

---
+++ Repo/spark/Learning/Batch/myfirstspark.py
@@ -0,0 +1,12 @@
+import findspark
+findspark.init()
+
+import pyspark # only run after findspark.init()
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
+
+df = spark.sql('''select 'spark' as hello ''')
+df.show()
+
+df = spark.sql('''select 'spark' as hello ''')
+df.count()
