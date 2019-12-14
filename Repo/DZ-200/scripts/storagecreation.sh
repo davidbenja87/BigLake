0936aeee026e0a0a0cb8f714412b84dc7787ec55
davidbenja87 <david.benja.87@gmail.com>
2019-12-14 13:15:56 +0000

    add storage

--- Repo/DZ-200/scripts/storagecreation.sh
+++ Repo/DZ-200/scripts/storagecreation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Hello World"
-RESOURCE_GROUP="DP-200"
+RESOURCE_GROUP="learn-88e67ece-28ac-4130-b7a2-01f49021e698"
 STORAGE_NAME="fileshareapp"
 az storage account create \
-        --resource-group "DP-200" \
+        --resource-group $RESOURCE_GROUP \
         --kind storageV2 \
         --sku Standard_LRS \
-        --access-tier Cool \
+        --access-tier Hot \
         --name "fileshareapp"
 echo "$STORAGE_NAME"
